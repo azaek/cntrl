43469b732115c0b76aba879f830df8b79b7272d8
AzaeK <aloxel@gmail.com>
2026-02-01 14:14:20 +0530

    opt(sdk): drop zod, move react-query to peerDependencies

--- apps/docs/src/mdx-components.tsx
+++ apps/docs/src/mdx-components.tsx
@@ -1,6 +1,7 @@
 import { Icon } from "@/components/icon";
 import { Callout } from "fumadocs-ui/components/callout";
 import { Step, Steps } from "fumadocs-ui/components/steps";
+import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
@@ -10,6 +11,8 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     Step,
     Steps,
     Callout,
+    Tab,
+    Tabs,
     Icon,
     ...components,
   };
